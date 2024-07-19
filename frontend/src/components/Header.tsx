e71d9618ef60f328af172d913a230ccbf4156bd7
nlb19 <noahbowman@gmail.com>
2024-07-19 16:11:39 -0500

    Basic vite react-ts install

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+function Header() { 
+    
+    return (
+        <div className="the-approach__header h-16 md:h-24 w-screen bg-green-800 fixed top-0">
+            <h1 className="text-white text-2xl md:text-3xl font-bold text-center">The Approach</h1>
+        </div>
+    );
+}
+export default Header;
